c52b835ddf8bf2f0806856c77cadd01364d69893
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-05-04 20:58:33 -0600

    animal product list and contents of basic_pasta

---
+++ src/restaurant.pl
@@ -0,0 +1,18 @@
+animal_product(lomito).
+animal_product(pollo).
+animal_product(pescado).
+animal_product(atún).
+animal_product(leche).
+animal_product(mantequilla).
+animal_product(queso).
+animal_product(huevo).
+animal_product(miel).
+
+contains(basic_pasta, pasta).
+contains(basic_pasta, orégano).
+contains(basic_pasta, tomate).
+contains(basic_pasta, aceite).
+contains(basic_pasta, sal).
+
+
+
