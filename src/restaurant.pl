d71c1fec8a3ca953a41c04432f0385f9d3166c5c
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-05-07 15:37:09 -0600

    added new clients clasification.

--- src/restaurant.pl
+++ src/restaurant.pl
@@ -19,6 +19,20 @@ special_dish_side(rice).
 special_dish_side(mushrooms).
 special_dish_side(zuccini).
 
+# TODO: change this to direct users with names
+# diet(vegan).
+# diet(vegetarian).
+# diet(gluten_free).
+# diet(lactose_free).
+# diet(carnivore).
+# EXAMPLE:
+omnivor(chiqui).
+mushroom_allergic(Israel).
+vegetarian(Carlos).
+vegan(Angie).
+lactose_free(Emilia).
+pasta_free(Luis).
+
 contains(basic_pasta, pasta).
 contains(basic_pasta, orégano).
 contains(basic_pasta, tomate).
@@ -38,6 +52,15 @@ contains(caesar_salad, vinagre).
 contains(ice_cream, leche).
 contains(ice_cream, miel).
 contains(ice_cream, fresas).
+
+can_eat_meal(person_diete, common_meal) :-
+    omnivor(person_diete);
+    (lactose_free(person_diete), not(contains(common_meal, leche)));
+
+can_eat_meal(person_diete, meal_main_dish, meal_side_dish) :-
+
+
+can_eat_ice_cream(person_diete) :-
 
 # what function to add?
 # can_eat_meal(Person, Meal) :- can_eat_meal(Person, Meal, []).
