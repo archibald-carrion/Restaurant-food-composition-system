3884f85f6c0801f3e3de22df58e91a758e7fa9cc
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-05-08 16:07:23 -0600

    refactoring, hongos changed to mushrooms.

--- src/restaurant.pl
+++ src/restaurant.pl
@@ -8,11 +8,16 @@ animal_product(queso).
 animal_product(huevo).
 animal_product(miel).
 
+not_animal_product(mushrooms).
+not_animal_product(potatoes).
+not_animal_product(rice).
+
+
 special_dish_main(lomito).
 special_dish_main(pollo).
 special_dish_main(pescado).
 special_dish_main(atún).
-special_dish_main(hongos).
+special_dish_main(mushrooms).
 
 special_dish_side(potatoes).
 special_dish_side(rice).
@@ -39,10 +44,10 @@ contains(basic_pasta, tomate).
 contains(basic_pasta, aceite).
 contains(basic_pasta, sal).
 
-contains(hongos_al_horno, hongos).
-contains(hongos_al_horno, queso).
-contains(hongos_al_horno, mantequilla).
-contains(hongos_al_horno, sal).
+contains(mushrooms_al_horno, mushrooms).
+contains(mushrooms_al_horno, queso).
+contains(mushrooms_al_horno, mantequilla).
+contains(mushrooms_al_horno, sal).
 
 contains(caesar_salad, lechuga).
 contains(caesar_salad, tomate).
