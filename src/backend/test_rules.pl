6e7bd1d4c113de66a68be7a9d89cef6610b40efa
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-06 11:50:35 -0600

    add test files used to test the prolog interpreter

---
+++ src/backend/test_rules.pl
@@ -0,0 +1,6 @@
+vegan_friendly(mushroom).
+vegetarian_friendly(cheese).
+animal_product_meat(chicken).
+can_eat_meal(vegan, mushroom, rice).
+can_eat_meal(vegetarian, mushroom, cheese).
+can_eat_meal(omnivor, chicken, rice).
